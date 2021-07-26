9ce64c40f104a8b462b6ca01afd107bd56b75f1b
RATHOD VINIL <viniljain703@gmail.com>
2021-07-26 10:33:09 +0530

    Delete config directory

--- config/index.js
+++
@@ -1,9 +0,0 @@
-const mysql = {
-    host     : 'freedb.tech',
-    user     : 'freedbtech_vinilchat',
-    password : 'zP8eexrf@5V4Rrg',
-    database : 'freedbtech_vichat'
-}
-module.exports={
-mysql
-}
